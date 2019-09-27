8a19f2994bf4376bb18ec1df52ecf50ab36165ee
Pavel Zagainov <pasha_zigzag@mail.ru>
2019-09-27 18:40:30 +1000

    регистрация

---
+++ register-form.php
@@ -0,0 +1,187 @@
+<?php 
+session_start();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Comments</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Nunito"
+      rel="stylesheet"
+    />
+
+    <!-- Styles -->
+    <link href="css/app.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div id="app">
+      <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+          <a class="navbar-brand" href="index.php">
+            Project
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto"></ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+              <!-- Authentication Links -->
+              <li class="nav-item">
+                <a class="nav-link" href="login.html">Login</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="register.html">Register</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <main class="py-4">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-md-8">
+              <div class="card">
+                <div class="card-header">Register</div>
+
+                <div class="card-body">
+                  <?php if (isset($_SESSION['flash_danger'])) : ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?= $_SESSION['flash_danger'] ?>
+                    </div>
+                  <?php unset($_SESSION['flash_danger']); ?>
+                  <?php endif; ?>
+
+                  <form method="POST" action="register.php">
+                    <div class="form-group row">
+                      <label
+                        for="name"
+                        class="col-md-4 col-form-label text-md-right"
+                        >Name</label
+                      >
+
+                      <div class="col-md-6">
+                        <input
+                          id="name"
+                          type="text"
+                          class="form-control"
+                          name="name"
+                          autofocus
+                        />
+                      </div>
+                      <?php if (isset($_SESSION['flash_user'])) : ?>
+                          <span class="red col-md-4 text-md-right">
+                              <?= $_SESSION['flash_user'] ?>
+                          </span>
+                          <?php unset($_SESSION['flash_user']); ?>
+                      <?php endif; ?>
+                    </div>
+
+                    <div class="form-group row">
+                      <label
+                        for="email"
+                        class="col-md-4 col-form-label text-md-right"
+                        >E-Mail Address</label
+                      >
+
+                      <div class="col-md-6">
+                        <input
+                          id="email"
+                          type="email"
+                          class="form-control"
+                          name="email"
+                        />
+                      </div>
+                      <?php if (isset($_SESSION['flash_email'])) : ?>
+                          <span class="red col-md-4 text-md-right">
+                              <?= $_SESSION['flash_email'] ?>
+                          </span>
+                          <?php unset($_SESSION['flash_email']); ?>
+                      <?php endif; ?>
+                    </div>
+
+                    <div class="form-group row">
+                      <label
+                        for="password"
+                        class="col-md-4 col-form-label text-md-right"
+                        >Password</label
+                      >
+
+                      <div class="col-md-6">
+                        <input
+                          id="password"
+                          type="password"
+                          class="form-control"
+                          name="password"
+                        />
+                      </div>
+                      <?php if (isset($_SESSION['flash_password'])) : ?>
+                          <span class="red col-md-4 text-md-right">
+                              <?= $_SESSION['flash_password'] ?>
+                          </span>
+                          <?php unset($_SESSION['flash_password']); ?>
+                      <?php endif; ?>
+                    </div>
+
+                    <div class="form-group row">
+                      <label
+                        for="password-confirm"
+                        class="col-md-4 col-form-label text-md-right"
+                        >Confirm Password</label
+                      >
+
+                      <div class="col-md-6">
+                        <input
+                          id="password-confirm"
+                          type="password"
+                          class="form-control"
+                          name="password_confirmation"
+                        />
+                      </div>
+                      <?php if (isset($_SESSION['flash_password_confirmation'])) : ?>
+                          <span class="red col-md-4 text-md-right">
+                              <?= $_SESSION['flash_password_confirmation'] ?>
+                          </span>
+                          <?php unset($_SESSION['flash_password_confirmation']); ?>
+                      <?php endif; ?>
+                    </div>
+
+                    <div class="form-group row mb-0">
+                      <div class="col-md-6 offset-md-4">
+                        <button type="submit" class="btn btn-primary">
+                          Register
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  </body>
+</html>
---
+++ register.php
@@ -0,0 +1,71 @@
+<?php
+    session_start();
+    
+    // Получаем данные из формы
+    $username = $_POST['name'];
+    $email = $_POST['email']; 
+    $password = $_POST['password']; 
+    $password_confirmation = $_POST['password_confirmation']; 
+
+    // Валидация данных
+    if(empty($username)) {
+        $_SESSION['flash_user'] = 'Введите имя пользователя';
+    }   elseif (strlen($username) > 15) {
+        $_SESSION['flash_user'] = 'Введите корректное имя';
+    }
+
+    if(empty($email)) {
+        $_SESSION['flash_email'] = 'Введите Email';
+    } 
+
+    if ($password_confirmation !== $password) {
+        $_SESSION['flash_password'] = 'Введенные пароли не совпадают';
+    }
+
+    if(empty($password)) {
+        $_SESSION['flash_password'] = 'Введите пароль';
+    } 
+
+    if(empty($password_confirmation)) {
+        $_SESSION['flash_password_confirmation'] = 'Введите пароль еще раз';
+    }
+
+    if ($password_confirmation !== $password) {
+        $_SESSION['flash_password'] = 'Введенные пароли не совпадают';
+    }
+
+    if( isset($_SESSION['flash_user']) || isset($_SESSION['flash_email']) || isset($_SESSION['flash_password']) || isset($_SESSION['flash_password_confirmation']) ) {
+        $_SESSION['flash_danger'] = 'Произошла ошибка!';
+
+        header('Location: register-form.php');
+        die;
+    }
+
+    //Хешируем пароль
+    $password = md5($password); 
+
+    // Соединяемся с базой
+    $driver = 'mysql'; // тип базы данных, с которой мы будем работать 
+    $host = 'localhost';// альтернатива '127.0.0.1' - адрес хоста, в нашем случае локального
+    $db_name = 'marlin-second'; // имя базы данных 
+    $db_user = 'root'; // имя пользователя для базы данных 
+    $db_password = ''; // пароль пользователя 
+    $charset = 'utf8'; // кодировка
+
+    $dsn = "$driver:host=$host;dbname=$db_name;charset=$charset";
+    $pdo = new PDO($dsn, $db_user, $db_password); 
+
+    $sql = 'INSERT INTO users (username, email, password) VALUES (:username, :email, :password)';
+    $stmt = $pdo->prepare($sql);
+    $result = $stmt->execute([  ':username'  => $username,
+                                ':email'     => $email,
+                                ':password'  => $password]);
+
+    if (!$result) {
+        var_dump($result);
+        die;
+    }
+
+    $_SESSION['flash_success'] = 'Вы успешно зарегестрировались!';
+
+    header('Location: login.php');
